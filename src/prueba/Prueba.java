cf5bbf044e08fd1ece3b1bb6549f6f1df21d0df5
saruedreams <saruedreams@192.168.0.102>
2020-09-27 19:28:45 +0200

    Cambio y borrado

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,8 +4,8 @@ public class Prueba {
 	
 	public static void man(String[] args) {
 		System.out.println("línea 1");
-		System.out.println("línea 1");
-		System.out.println("línea 1");
+		System.out.println("línea 2");
+		System.out.println("línea 3");
 	}
 
 }
