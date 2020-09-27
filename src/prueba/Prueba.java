5996cd052b1542f85c26175ff827e6d65d790419
saruedreams <saruedreams@192.168.0.102>
2020-09-27 20:11:31 +0200

    Conflicto arreglado

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,11 +4,7 @@ public class Prueba {
 	
 	public static void man(String[] args) {
 		System.out.println("línea 1");
-<<<<<<< HEAD
-		System.out.println("este texto es distinto");
-=======
-		System.out.println("esta línea sigue igual");
->>>>>>> refs/heads/master
+		System.out.println("solucionado");
 		System.out.println("línea 3");
 	}
 
