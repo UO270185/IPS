b957e5fa88604b46b0ce0f1df059665645c0d439
saruedreams <saruedreams@192.168.0.102>
2020-09-27 20:16:08 +0200

    Rama rebase añadida

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -3,9 +3,7 @@ package prueba;
 public class Prueba {
 	
 	public static void man(String[] args) {
-		System.out.println("línea 1");
-		System.out.println("solucionado");
-		System.out.println("línea 3");
+		System.out.println("rebase");
 	}
 
 }
--- src/prueba/Prueba2.java
+++ src/prueba/Prueba2.java
@@ -4,9 +4,9 @@ public class Prueba2 {
 
 	public static void man(String[] args) {
 		System.out.println("línea 1");
-		System.out.println("línea 1");
-		System.out.println("línea 1");
-		System.out.println("línea 1");
-		System.out.println("línea 1");
+		System.out.println("línea 2");
+		System.out.println("línea 3");
+		System.out.println("línea 4");
+		System.out.println("línea 5");
 	}
 }
