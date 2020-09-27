4e38ab1475db442a7b2eb868f501257fe2a568e9
saruedreams <saruedreams@192.168.0.102>
2020-09-27 20:22:48 +0200

    Conflicto arreglado

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -3,16 +3,7 @@ package prueba;
 public class Prueba {
 	
 	public static void man(String[] args) {
-<<<<<<< HEAD
-		System.out.println("rebase");
-		System.out.println("sigo haciendo cambios sin miedo a nada");
-=======
-		System.out.println("línea 1");
-		System.out.println("solucionado");
-		System.out.println("línea 3");
-		System.out.println("más cosas se han cambiado por aquí");
-		System.out.println("alguien más ha cambiado cosas");
->>>>>>> refs/heads/master
+		System.out.println("arreglado el conflicto entre master y rebase");
 	}
 
 }
