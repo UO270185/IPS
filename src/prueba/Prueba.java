0f157970a7d56bb3585169de613ae43ce84b50fd
saruedreams <saruedreams@192.168.0.102>
2020-09-27 19:55:51 +0200

    cambio en master

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 	
 	public static void man(String[] args) {
 		System.out.println("línea 1");
-		System.out.println("línea 2");
+		System.out.println("esta línea sigue igual");
 		System.out.println("línea 3");
 	}
 
