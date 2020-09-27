e7e044ef4f868c299eb159ba6677bab9ba63324d
saruedreams <saruedreams@192.168.0.102>
2020-09-27 20:48:43 +0200

    Cambio en master 1

--- src/prueba/Prueba2.java
+++ src/prueba/Prueba2.java
@@ -5,8 +5,5 @@ public class Prueba2 {
 	public static void man(String[] args) {
 		System.out.println("línea 1");
 		System.out.println("línea 1");
-		System.out.println("línea 1");
-		System.out.println("línea 1");
-		System.out.println("línea 1");
 	}
 }
