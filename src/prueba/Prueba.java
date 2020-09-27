290d6de60419121ebc5224617447c2b1be1d8073
saruedreams <saruedreams@192.168.0.102>
2020-09-27 20:40:02 +0200

    Rebase actualizado

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,6 +4,7 @@ public class Prueba {
 	
 	public static void man(String[] args) {
 		System.out.println("arreglado el conflicto entre master y rebase");
+		System.out.println("añado más cosas");
 	}
 
 }
